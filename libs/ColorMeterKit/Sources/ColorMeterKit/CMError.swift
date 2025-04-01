672e36ae4cef9e5fcbedcd773310d93d139be7d7
Fernando Castillo <fcastillo@seagullscientific.com>
2025-04-01 15:51:37 -0500

    add sdk

---
+++ libs/ColorMeterKit/Sources/ColorMeterKit/CMError.swift
@@ -0,0 +1,29 @@
+//
+//  File.swift
+//  
+//
+//  Created by chenlongmingob@gmail.com on 2020/12/22.
+//
+
+import Foundation
+
+
+public enum CMError: Error {
+    
+    // MARK: - bluetooth errors
+    case peripheralDisconnect
+    case characteristicNotFound
+    case failToConnect
+    case invalidResponseData
+    case responseTimeout
+    case storageIndexOutOfRange
+    case getStorageFailure
+    case executingCommand
+    case calibrateFailure
+    
+    
+    // MARK: - color formatter errors
+    case invalidSpectralWaveInterval
+    
+    case unknown
+}
